0f1ef1b21d319b3ce24053c23af17a7018f1f37a
Chaway <673262173@qq.com>
2016-04-04 15:35:00 +0800

    a demo

---
+++ prime.cpp
@@ -0,0 +1,15 @@
+#include <iostream.h>
+using namespace std;
+int main()
+{
+	int MaxN;
+	cin >> MaxN;
+	cout << "MaxN = " << MaxN << endl;
+    bool isNotprime[MaxN/2] = {0};
+    int iMax = (int)(sqrt(MaxN)/2);
+    for (int i = 0; i < count; ++i)
+    {
+    	
+    }
+	return 0;
+}
