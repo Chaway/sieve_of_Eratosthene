e31d7f0c04db6e26d5218fd5728ee154c00239d0
Chaway <673262173@qq.com>
2016-04-04 16:42:31 +0800

    done

--- prime.cpp
+++ prime.cpp
@@ -1,15 +1,33 @@
-#include <iostream.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
 	int MaxN;
 	cin >> MaxN;
 	cout << "MaxN = " << MaxN << endl;
-    bool isNotprime[MaxN/2] = {0};
-    int iMax = (int)(sqrt(MaxN)/2);
-    for (int i = 0; i < count; ++i)
+	int primelength = (MaxN - 1) / 2;  
+	//cout << primelength << endl;
+  /*
+	for example:when MaxN = 99 , imax = 48 , length = 49;
+	            when MaxN = 100 , imax = 48 , length = 49;
+  */
+    bool isNotprime[primelength] = {0};  //3,5,7,9,11,...,
+    int iMax = (int)((sqrt(MaxN) - 3)/2);
+
+    for (int i = 0; i <= iMax ; ++i)
     {
-    	
+      if(!isNotprime[i])
+      for (int j = 2*i*i + 6*i + 3; j <= primelength ; j += 2*i + 3)
+       {
+       //	cout << j << endl;
+    	isNotprime[j] = 1;
+       }
     }
+
+    for (int i = 0; i < primelength; ++i)
+		if(!isNotprime[i])
+		 cout << 2*i + 3 << endl;
 	return 0;
+  
 }
